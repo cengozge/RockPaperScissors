df6da5872bd8b8995b539b5b4fadbea43e6f84e5
ozge <ozge@Ozge>
2017-04-08 21:08:26 +0300

    sixth

---
+++ src/main/java/RockPaperScissors/enums/INPUT.java
@@ -0,0 +1,13 @@
+package RockPaperScissors.enums;
+
+public enum INPUT {
+
+	ROUND("Round"),
+	MATERIAL_TYPE("Material");
+	
+	private String code;
+
+	INPUT(String code) {
+		this.code = code;
+	}
+}
