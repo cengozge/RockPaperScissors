2a75e61a3db807a005a43716b03271427e325135
ozge <ozge@Ozge>
2017-04-08 21:47:35 +0300

    sixth

--- src/main/java/RockPaperScissors/Controller.java
+++ src/main/java/RockPaperScissors/Controller.java
@@ -3,45 +3,58 @@ package RockPaperScissors;
 import java.util.Scanner;
 
 import RockPaperScissors.entity.Player;
-import RockPaperScissors.enums.INPUT;
 import RockPaperScissors.enums.MATERIAL;
 
 public class Controller {
 
 	private static int round = 0;
 	private static int tie = 0;
-	
+
+	private static Player playerA = new Player();
+	private static Player playerB = new Player();
+
 	public static void manageGame() {
-		
-		getInputFromUser(INPUT.ROUND);
-		
-		if(round <= 0){
+
+		getRoundFromUser();
+
+		if (round <= 0) {
 			throw new RuntimeException("Round input must be a positive number. ");
 		}
-		
-		Player playerA = new Player();
-		Player playerB = new Player();
-		playerA.setMaterial(MATERIAL.PAPER);
-		
+
 		while (round > 0) {
-			
-			String choiceOfPlayerB = getInputFromUser(INPUT.MATERIAL_TYPE);
-			playerB.setMaterial(MATERIAL.fromCode(choiceOfPlayerB));
+			createPlayers();
 			playOneRound(playerA, playerB);
-			
 		}
-		
+
 		printResult(playerA, playerB);
 	}
-	
-	public static void printResult(Player playerA, Player playerB){
-		System.out.println("Player A wins: " + playerA.getScore() + " rounds");
-		System.out.println("Player B wins: " + playerB.getScore() + " rounds");
-		System.out.println("Tie: " + tie);
+
+	public static void getRoundFromUser() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter how many rounds you want to play: ");
+		setRound(s.nextInt());
 	}
-	
-	public static void playOneRound(Player playerA, Player playerB) {
+
+	public static String getMaterialFromUser() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter p for Paper, s for Scissors, r for Rock: ");
+		String choiceOfPlayerB = s.nextLine();
+		if(round == 1){
+			s.close();
+		}
+		return choiceOfPlayerB;
+	}
+
+	public static void createPlayers() {
 		
+		playerA.setMaterial(MATERIAL.PAPER);
+
+		String choiceOfPlayerB = getMaterialFromUser();
+		playerB.setMaterial(MATERIAL.fromCode(choiceOfPlayerB));
+	}
+
+	public static void playOneRound(Player playerA, Player playerB) {
+
 		String winner = null;
 		if (playerA.getMaterial().isBeats(playerB.getMaterial())) {
 			playerA.win();
@@ -57,21 +70,11 @@ public class Controller {
 		System.out.println(winner + " wins! " + playerA.getScore() + " " + playerB.getScore());
 		round--;
 	}
-	
-	public static String getInputFromUser(INPUT input){
-		Scanner s = new Scanner(System.in);
-		if(input.equals(INPUT.ROUND)){
-			System.out.println("Please enter how many rounds you want to play: ");
-			setRound(s.nextInt());
-		} else if(input.equals(INPUT.MATERIAL_TYPE)){
-			System.out.println("Please enter p for Paper, s for Scissors, r for Rock: ");
-			String choiceOfPlayerB = s.nextLine();
-			if(getRound() == 1){
-				s.close();
-			}
-			return choiceOfPlayerB;
-		}
-		return "";
+
+	public static void printResult(Player playerA, Player playerB) {
+		System.out.println("Player A wins: " + playerA.getScore() + " rounds");
+		System.out.println("Player B wins: " + playerB.getScore() + " rounds");
+		System.out.println("Tie: " + tie);
 	}
 
 	public static int getRound() {
@@ -81,7 +84,5 @@ public class Controller {
 	public static void setRound(int round) {
 		Controller.round = round;
 	}
-	
-	
-	
+
 }
